ce3bf13d10d2bf018808cd81158cb3670d644984
Ossy Lab <ossyisibor@gmail.com>
2023-05-14 20:46:53 +0100

    completed the header and the search form

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  //   plugins: [
+  //     new HtmlWebpackPlugin({
+  //       template: path.resolve(__dirname, "./dist", "index.html"),
+  //     }),
+  //   ],
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+  },
+};
